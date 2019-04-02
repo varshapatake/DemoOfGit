2377632f425aa10b7ca67fa69dd9904a339ebd0e
Varsha Patake <patakevarsha@gmail.com>
2019-04-02 11:53:59 +0530

    first commit in git

---
+++ src/test/java/com/qa/tests/TestHomePage.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+
+
+public class TestHomePage {
+	@Test
+	public void test_home()
+	{
+		System.out.println("home page test");
+	}
+}
